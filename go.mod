262b8393f1e0c69b20e4a486e3bd3c86c3b8b926
Jase Huang <jase@Jases-MacBook-Pro.local>
2019-11-16 16:44:21 +0800

    first server

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.13
 require (
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/pkg/errors v0.8.1
 	golang.org/x/crypto v0.0.0-20191112222119-e1110fd1c708 // indirect
 )
--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"agr-hack/internal/errors"
+	"agr-hack/internal/format"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
+	"runtime"
 	"time"
 
 	"github.com/labstack/echo"
@@ -15,6 +19,7 @@ const (
 
 	oneWeekWeather = "https://opendata.cwb.gov.tw/fileapi/v1/opendataapi/F-A0010-001?Authorization=CWB-B598382E-A64D-4809-B598-5C434E4FCEAB&downloadType=WEB&format=JSON"
 )
+
 type Fields map[string]interface{}
 
 func main() {
@@ -25,7 +30,7 @@ func main() {
 		ReadTimeout:  1 * time.Minute,
 		WriteTimeout: 1 * time.Minute,
 	}
-	e.Debug = false 
+	e.Debug = false
 	e.HTTPErrorHandler = errors.HTTPErrorHandlerForEcho
 	// cover all api error response
 	e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
@@ -50,7 +55,7 @@ func main() {
 	})
 
 	// route
-	e.POST("/upload", upload)
+	e.GET("/", ping)
 
 	// test file
 	dst, err := os.Create("test")
@@ -70,4 +75,34 @@ func main() {
 	log.Println("local ip address :", format.GetCMDColor(format.Color_green, address))
 	// start
 	e.Logger.Fatal(e.StartServer(s))
+}
+
+func ping(c echo.Context) error {
+	return c.JSON(http.StatusOK, "pin pin~~~poooooon!")
+}
+
+func getIP() (string, error) {
+	ip := ""
+	switch runtime.GOOS {
+	case "windows":
+		cmd := exec.Command("ipconfig")
+		d, err := cmd.CombinedOutput()
+		if err != nil {
+			return "", err
+		}
+		ip = string(d)
+	case "linux", "darwin":
+		cmd := exec.Command("bash", "-c", "ifconfig | grep -Eo 'inet (addr:)?([0-9]*\\.){3}[0-9]*' | grep -Eo '([0-9]*\\.){3}[0-9]*' | grep -v '127.0.0.1'")
+		d, err := cmd.CombinedOutput()
+		if err != nil {
+			return "", err
+		}
+		ip = string(d)
+	}
+	// ip has content
+	if ip != "" {
+		log.Println(ip)
+		return ip, nil
+	}
+	return "", errors.New("your os is " + runtime.GOOS + " not support get ip command")
 }
